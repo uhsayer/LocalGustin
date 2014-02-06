dac2dd63f86b3bdfac84ca3cbfa291d925e0690e
uhsayer <chenalbertyu@gmail.com>
2014-02-06 16:18:20 -0500

    message here

---
+++ fubar/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Fubar::Application.config.secret_key_base = '40a63099411b642bbcfe2ed25ac1ab57085baa51f6866bb3d171209595459e027c8f3de2c0b217093401bf68b9b714ce6f827c6daac50655fdbaf8ef9f08ad1b'
---
+++ fubar/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Fubar::Application.config.session_store :cookie_store, key: '_fubar_session'
